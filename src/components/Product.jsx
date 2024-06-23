dbca678e8f6767d90d5570959c8db5cd761377ea
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-06-23 23:29:51 +0300

    add props

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,23 @@
-const Product = () => {
-  const price = 999;
-  const imgUrl = "<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>";
+// const Product = () => {
+//   const price = 999;
+//   const imgUrl = "<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>";
 
-  return (
-    <>
-      <h2>Tacos</h2>
-      <img src={imgUrl} />
+//   return (
+//     <>
+//       <h2>Tacos</h2>
+//       <img src={imgUrl} />
+// 			<p>Price: {price} credits</p>
+//     </>
+//   );
+// };
+
+// export default Product;
+
+export const Product = ({name, imgUrl, price}) => 
+  (
+    <div>
+      <h2>{name}</h2>
+			<img src={imgUrl} alt={name} width="480" />
 			<p>Price: {price} credits</p>
-    </>
+    </div>
   );
-};
-
-export default Product;
